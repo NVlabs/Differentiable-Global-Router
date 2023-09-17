4f625123e192da513b2d5d13080ff6dbb664382e
Wei Li <weili3@moura-rtx.ece.local.cmu.edu>
2023-09-16 21:48:51 -0400

    update new tree script

--- New_Tree.sh
+++ New_Tree.sh
@@ -21,20 +21,26 @@ cugr2=$3
 this_path=$4
 method="NewTree"
 # first round CUGR, generating fine-tuned tree
+cd $cugr2/run/
 ./route -lef $benchmark_path/$data/$data.input.lef -def $benchmark_path/$data/$data.input.def -output $benchmark_path/$data/$data.CUGR2_NewSort.guide -sort 1 > $cugr2/GR_log/$data\_CUGR2_NewSort.log
 
+cd $this_path
 # first round DGR and second round CUGR2, generating fine-tuned tree
 python3 main_stochastic.py --data_path $cugr2/run/$data.pt --output_name FirstRound
+
 cd $cugr2/run/
 ./route -lef $benchmark_path/$data/$data.input.lef -def $benchmark_path/$data/$data.input.def -output $benchmark_path/$data/$data.FirstRound.guide -dgr $this_path/CUGR2_guide/CUgr_$data\_FirstRound.txt > $cugr2/GR_log/$data\_FirstRound.log
 
 # second round DGR and third round CUGR2, generating final result
+cd $this_path
 python3 main_stochastic.py --data_path $cugr2/run/$data.pt  --read_new_tree True --output_name $method
+
 cd $cugr2/run/
 ./route -lef $benchmark_path/$data/$data.input.lef -def $benchmark_path/$data/$data.input.def -output $benchmark_path/$data/$data.$method.guide -dgr $this_path/CUGR2_guide/CUgr_$data\_$method.txt -tree $this_path/CUGR2_guide/CUgr_$data\_tree.txt > $cugr2/GR_log/$data\_$method.log
 
 # Run Detailed Routing
 cd $cugr2
 ./drcu -lef $benchmark_path/$data/$data.input.lef -def $benchmark_path/$data/$data.input.def -thread 32 -guide $benchmark_path/$data/$data.$method.guide --output $cugr2/DR_result/${method}_$data.txt --tat 2000000000 > $cugr2/DR_log/$data\_$method.log
+
 cd $this_path
 sh run_innovus.sh $data $method
